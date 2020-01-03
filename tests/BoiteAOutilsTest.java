6752770e8d431731e5e63f42ce5ab2d6c16a8e45
florian <gigotFlorian@gmail.com>
2020-01-03 17:35:54 +0100

    Boite a outils Test

--- tests/BoiteAOutilsTest.java
+++ tests/BoiteAOutilsTest.java
@@ -1,5 +1,28 @@
+import controlleur.BoiteAOutil;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoiteAOutilsTest {
 
+
+    @Test
+    public void supprimerEspaceTest() {
+        String [] str = {"aa( )", " aze ", " az","a&a ","aa((    ))&aa","a"};
+        String [] strResult = {"aa( )", "aze", "az","a&a","aa((    ))&aa","a"};
+
+        for(int i =0 ; i<str.length; i++){
+            assertEquals(BoiteAOutil.supprimerEspace(str[i]), strResult[i]);
+        }
+    }
+
+    @Test
+    public void formaterCasseTest() {
+        String [] str ={"Antoine cARL","aa BB", "anToiNe BlAbLa", "antoine blabla","AA"};
+        String [] strResult ={"Antoine Carl","Aa Bb","Antoine Blabla","Antoine blabla","Aa"};
+        for(int i =0 ; i<str.length; i++){
+            assertEquals(BoiteAOutil.formaterCasse(str[i]), strResult[i]);
+        }
+
+    }
 }
