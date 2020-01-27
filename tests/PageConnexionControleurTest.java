63b2755813f16732ffd20d1a7eb3651345b5568c
florian <gigotFlorian@gmail.com>
2020-01-27 17:46:28 +0100

    Test page de connection ( trouver le bon choix de librairies pour ne pas avoir de pb de compatibilité)

---
+++ tests/PageConnexionControleurTest.java
@@ -0,0 +1,40 @@
+import controlleur.SetupEM;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+import sample.Main;
+
+public class PageConnexionControleurTest extends ApplicationTest {
+    @Override
+    public void start(Stage stage) throws Exception {
+        new SetupEM();
+        Parent mainNode = FXMLLoader.load(Main.class.getResource("connexion.fxml"));
+        stage.setScene(new Scene(mainNode));
+        stage.show();
+        stage.toFront();
+    }
+    @Before
+    public void setUp () throws Exception{
+
+    }
+    @After
+    public void tearDown () throws Exception {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+    @Test
+    public void cliquerSurSeConnecterSansNomUtilisateur(){
+
+        clickOn("#textField");
+        write("");
+    }
+}
